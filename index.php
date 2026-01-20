6885764942865f422b3505d55753aa480a5c949d
ALIF IMAN BIN ROSLAN <alifimanroslan06@gmail.com>
2026-01-20 15:33:29 +0800

    kemaskini code

--- index.php
+++ index.php
@@ -2,24 +2,5 @@
 $nama = "ALIF IMAN ROSLAN";
 $nomatrik = "18DIT24F1083";
 $kelas = "DIT4C";
+$program = "DFP40443 Full Stack Web Development";
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Maklumat Diri</title>
-</head>
-<body>
-
-<h1>Maklumat Pelajar</h1>
-
-<img src="profile.jpeg" width="200">
-
-<p><strong>Nama:</strong> <?= $nama ?></p>
-<p><strong>No Matrik:</strong> <?= $nomatrik ?></p>
-<p><strong>Kelas:</strong> <?= $kelas ?></p>
-
-<p>Program: DFP40443 Full Stack Web Development</p>
-
-</body>
-</html>
