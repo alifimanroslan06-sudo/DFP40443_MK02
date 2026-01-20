6f5fe2b20c030b52bf546b7383580b4d14d4b3b1
alifimanroslan06-sudo <alifimanroslan06@gmail.com>
2026-01-20 14:32:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$nama = "ALIF IMAN ROSLAN";
+$nomatrik = "18DIT24F1083";
+$kelas = "DIT4C";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Maklumat Diri</title>
+</head>
+<body>
+
+<h1>Maklumat Pelajar</h1>
+
+<img src="profile.jpeg" width="200">
+
+<p><strong>Nama:</strong> <?= $nama ?></p>
+<p><strong>No Matrik:</strong> <?= $nomatrik ?></p>
+<p><strong>Kelas:</strong> <?= $kelas ?></p>
+
+<p>Program: DFP40443 Full Stack Web Development</p>
+
+</body>
+</html>
